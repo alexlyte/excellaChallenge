5d14450cb5633b1f945e7725844147c7ed34f3d2
alexlyte <alexanderlyte@gmail.com>
2016-01-30 09:43:36 -0500

    added anagram

--- app/routes.js
+++ app/routes.js
@@ -16,11 +16,60 @@ module.exports = function(app) {
         console.log(params)
         res.json('found it!')
 
+
+
+
+
+    });
+
+    app.post('/api/anagram', function(req, res) {
+        // use mongoose to get all nerds in the database
+        var params = req.body;
+        var lists = params;
+        var resList = [];
+        lists.forEach(function(set){
+            var str1 = set[0].split('');
+            var str2 = set[1].split('');
+
+            // for each letter in the first string
+            var flag = true;
+            str1.forEach(function(let){
+                // if that letter is in the second string
+                // return true
+                // else return false
+                console.log(let)
+                if(str2.indexOf(let) === -1){
+                    flag = false;
+                }
+            })
+
+            if(flag){
+                resList.push(true)
+            } else {
+                resList.push(false)
+            }
+
+            // for each string, get an array of letters
+            // if the array of ordered letters in set0 is the same as in set 1, return true
+
+
+
+            console.log(str1)
+            console.log(str2)
+            console.log(flag)
+            console.log('\n')
+        })
+
+
+        res.json({'data' : resList})
+
         
 
 
 
     });
+
+
 
     // frontend routes =========================================================
     // route to handle all angular requests
